1d55710a0ba3f9780de6705cf1e04e087ef3778d
allegroai <>
2021-05-03 17:33:12 +0300

    Update max API version

--- apiserver/service_repo/service_repo.py
+++ apiserver/service_repo/service_repo.py
@@ -32,7 +32,7 @@ class ServiceRepo(object):
     """If the check is set, parsing will fail for endpoint request with the version that is grater than the current 
     maximum """
 
-    _max_version = PartialVersion("2.12")
+    _max_version = PartialVersion("2.13")
     """ Maximum version number (the highest min_version value across all endpoints) """
 
     _endpoint_exp = (
