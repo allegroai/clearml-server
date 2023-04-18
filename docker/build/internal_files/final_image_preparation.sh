c8e4d9eeacc8e9515442623458d039283054f9d6
allegroai <>
2023-04-18 10:50:13 +0300

    Fix Dockerfile uses deprecated base image

--- docker/build/internal_files/final_image_preparation.sh
+++ docker/build/internal_files/final_image_preparation.sh
@@ -7,6 +7,7 @@ yum update -y
 yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
 yum install -y python36 python36-pip nginx gcc gcc-c++ python3-devel gettext
 yum -y upgrade
+python3 -m pip install -U pip
 python3 -m pip install -r /opt/clearml/fileserver/requirements.txt
 python3 -m pip install -r /opt/clearml/apiserver/requirements.txt
 mkdir -p /opt/clearml/log
@@ -16,4 +17,5 @@ ln -s /dev/stderr /var/log/nginx/error.log
 mv /etc/nginx/nginx.conf /etc/nginx/nginx.conf.orig
 mv /tmp/internal_files/clearml.conf.template /etc/nginx/clearml.conf.template
 mv /tmp/internal_files/clearml_subpath.conf.template /etc/nginx/clearml_subpath.conf.template
+pip cache purge
 yum clean all
