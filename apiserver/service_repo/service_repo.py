d8b998ebd8d8240c5ce769f8302cd3a7a326a9e1
allegroai <>
2023-05-25 19:33:37 +0300

    Bump API version to 2.25

--- apiserver/service_repo/service_repo.py
+++ apiserver/service_repo/service_repo.py
@@ -39,7 +39,7 @@ class ServiceRepo(object):
     """If the check is set, parsing will fail for endpoint request with the version that is grater than the current 
     maximum """
 
-    _max_version = PartialVersion("2.24")
+    _max_version = PartialVersion("2.25")
     """ Maximum version number (the highest min_version value across all endpoints) """
 
     _endpoint_exp = (
